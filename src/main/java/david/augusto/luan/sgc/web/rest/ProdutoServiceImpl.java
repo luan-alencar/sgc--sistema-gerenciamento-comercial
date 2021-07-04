91b9984f7f2a0c9df8ae0af496592ad0746eeaf4
luan-alencar <luanalencar134@gmail.com>
2021-07-04 14:12:22 -0300

    ProdutoServiceImpl

---
+++ src/main/java/david/augusto/luan/sgc/web/rest/ProdutoServiceImpl.java
@@ -0,0 +1,41 @@
+package david.augusto.luan.sgc.web.rest;
+
+import david.augusto.luan.sgc.dominio.Categoria;
+import david.augusto.luan.sgc.service.ProdutoService;
+import david.augusto.luan.sgc.service.dto.ProdutoDTO;
+import david.augusto.luan.sgc.service.exceptions.RegraNegocioException;
+
+import java.util.List;
+
+public class ProdutoServiceImpl implements ProdutoService {
+
+    @Override
+    public List<ProdutoDTO> buscarTodos() {
+        return null;
+    }
+
+    @Override
+    public List<Categoria> obterPoCategoria(Integer idCategoria) {
+        return null;
+    }
+
+    @Override
+    public ProdutoDTO salvar(ProdutoDTO entity) {
+        return null;
+    }
+
+    @Override
+    public void delete(ProdutoDTO entity) {
+
+    }
+
+    @Override
+    public ProdutoDTO buscarPorId(Integer id) throws RegraNegocioException {
+        return null;
+    }
+
+    @Override
+    public ProdutoDTO atualizar(Integer id) throws RegraNegocioException {
+        return null;
+    }
+}
